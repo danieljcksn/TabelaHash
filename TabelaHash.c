8bc84a8e3a9d34844cb0320d564fd51a646bb4e8
daniielcavallcante <daniiel.cavallcante@gmail.com>
2019-12-14 16:16:56 +0000

    Tudo pronto! :D

--- TabelaHash.c
+++ TabelaHash.c
@@ -2,20 +2,63 @@
 #include <stdlib.h>
 #include <locale.h>
 
-//Protótipos das funções
-int indiceHashing(int elemento);
+//----------------* P R O T Ó T I P O S *----------------//
+int hashing(int elemento, int m);
 void tabelaHash(void);
+void buscaHash(int elemento, int* tabela, int m);
+void removeHash(int elemento, int* tabela, int m);
+void imprimeHash(int* tabela, int m);
+
+
+//----------------* F U N Ç Õ E S  D A  T A B E L A  H A S H *----------------//
+void imprimeHash(int* tabela, int m){
+	int i;
+	printf(" - - - T A B E L A - - - \n");
+	for(i = 0; i < m; i++){
+		if(tabela[i] == -1){
+			printf("[Nulo]\n");
+			continue;
+		}
+		printf("[%d]\n", tabela[i]);
+	}
+	printf(" - - - - - - - - - - - - \n\n\n\n");
+}
+
+//Função que remove um determinado elemento da tabela.
+void removeHash(int elemento, int* tabela, int m){
+	int i;
+	for(i=0; i<m; i++){
+		if(tabela[i] == elemento){
+			tabela[i] = -1;
+			printf("Elemento removido com sucesso!\n\n\n\n\n");
+			return;
+		}
+	}
+	printf("Elemento não encontra-se na tabela!\n\n\n\n\n");
+}
+
+//Função que busca na tabela um elemento e indica o seu índice.
+void buscaHash(int elemento, int* tabela, int m){
+	int i;
+	for(i=0; i<m; i++){
+		if(tabela[i] == elemento){
+			printf("Elemento encontrado! O seu índice é: %d\n\n\n\n\n", i);
+			return;
+		}
+	}
+	printf("Elemento não encontra-se na tabela!\n");
+}
 
 //Função que calcula o índice no qual será inserido um dado elemento.
-int indiceHashing(int elemento){
+int hashing(int elemento, int m){
 	int indice;
-	indice = elemento % 10;
+	indice = elemento % m;
 	return indice;
 }
 
 //Função principal que criará a tabela hash.
 void tabelaHash(void){
-	int m, i, j, elemento, indice, colisoes, iteracoes;
+	int m, i, j, elemento, indice, colisoes, iteracoes, escolha = 1, elem;
 
 	printf("Insira o tamanho da tabela: ");
 	scanf("%d", &m);
@@ -28,8 +71,9 @@ void tabelaHash(void){
 	for(i = 0; i < m; i++){
 		tabela[i] = -1;
 	}
-	printf("Agora, insira os %d elementos da tabela.", m);
+	printf("Agora, insira os %d elementos da tabela.\n", m);
 	printf("Para finalizar a inserção de elementos, insira -1\n");
+	printf("- - - - - - - - - - - - - - - - - - \n");
 	colisoes = 0;
 	for(i = 0; i < m; i++){
 		printf("Insira o %dº elemento da tabela: ", i+1);
@@ -38,11 +82,11 @@ void tabelaHash(void){
 		if(elemento == -1)
 			break;
 
-		indice = indiceHashing(elemento);
+		indice = hashing(elemento, m);
 
-		if(tabela[indice] == -1){
+		if(tabela[indice] == -1)
 			tabela[indice] = elemento;
-		}else{
+		else{
 			colisoes++;
 			iteracoes = 1;
 			for(j = indice+1; j < m; j++){
@@ -64,19 +108,66 @@ void tabelaHash(void){
 			}
 		}	
 	}
+	printf("- - - - - - - - - - - - - - - - - - \n");
+	printf("Número de colisões: %d\n\n", colisoes);
 
-	for(i = 0; i < m; i++){
-		if(tabela[i] == -1){
-			printf("[Nulo]\n");
-			continue;
+
+	//Menu de opções para o programa.
+	while(escolha != 4){
+		printf("Selecione uma das seguintes operações:\n");
+		printf("1 - Buscar elemento.\n");
+		printf("2 - Remover elemento.\n");
+		printf("3 - Imprimir tabela.\n");
+		printf("4 - Nova tabela.\n");
+		printf("5 - Sair.\n\n- - - - - - - - - - - - - - - - - - \n");	
+		printf("Sua escolha: ");
+		scanf("%d", &escolha);
+		switch (escolha){
+			case 1:
+				//Comando para limpar a tela.
+				printf("\e[H\e[2J");
+			    printf("Digite o elemento a ser buscado na tabela: ");
+			    scanf("%d", &elem);
+			    buscaHash(elem, tabela, m);
+				break;
+
+		   	case 2:
+		   		printf("\e[H\e[2J");
+			    printf("Digite o elemento a ser removido na tabela: ");
+			    scanf("%d", &elem);
+			    removeHash(elem, tabela, m);
+				break;
+
+		   	case 3:
+		   		printf("\e[H\e[2J");
+			    imprimeHash(tabela, m);
+				break;
+
+		   	case 4:
+		   		printf("\e[H\e[2J");
+		   		tabelaHash();
+			    return;
+				break;		
+
+			case 5:
+				printf("\e[H\e[2J");
+		   		printf("Tchau! :)\n\n\n\n");
+			    return;
+				break;
+
+			default:
+			    printf("Opção inválida!\n");
 		}
-		printf("[%d]\n", tabela[i]);
 	}
-	printf("Número de colisões: %d\n", colisoes);
-}
+	return;
+}	
 
+//----------------* F U N Ç Ã O  P R I N C I P A L *----------------//
 int main(void){
+
 	setlocale(LC_ALL, "Portuguese");
+	printf("\e[H\e[2J");
+	
 	tabelaHash();
 
 	return 0;
